0516a9be2bd7dab32bb8f590d929e97f6cee3a3d
Ankur Garg <ankur.bmv@gmail.com>
2024-10-20 13:32:33 +0530

    Initial project bootstrap commit.

---
+++ src/main/java/com/ecs/sgws/poc/romsdocuments/RomsDocumentsApplication.java
@@ -0,0 +1,13 @@
+package com.ecs.sgws.poc.romsdocuments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomsDocumentsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RomsDocumentsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ecs/sgws/poc/romsdocuments/RomsDocumentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecs.sgws.poc.romsdocuments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RomsDocumentsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
